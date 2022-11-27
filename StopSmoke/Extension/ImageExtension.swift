23b1d3486ac759db1578bd0199fa5a9a1bc46ceb
Whmara <Whmara00@gmail.com>
2022-11-27 23:35:05 +0300

    IMPLEMENTED floating action button

---
+++ StopSmoke/Extension/ImageExtension.swift
@@ -0,0 +1,33 @@
+//
+//  ImageExtension.swift
+//  StopSmoke
+//
+//  Created by Marat on 27.11.2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    
+    static var buttonOpenImage: UIImage {
+        UIImage(systemName: "ellipsis")!
+    }
+    
+    static var buttonCloseImage: UIImage {
+        UIImage(systemName: "xmark")!
+    }
+    
+    static var chartImage: UIImage {
+        UIImage(systemName: "chart.bar")!
+    }
+    
+    static var calendarImage: UIImage {
+        UIImage(systemName: "calendar")!
+    }
+    
+    static var heartImage: UIImage {
+        UIImage(systemName: "heart.text.square")!
+    }
+    
+}
